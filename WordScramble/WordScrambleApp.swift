c99fd40039a08461b41d8ef7cd02c0cf208a84ab
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-11-01 14:10:51 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Dmitry Sharabin on 01.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
