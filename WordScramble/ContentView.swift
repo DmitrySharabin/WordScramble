adf30de4f4e34d1947aa0693965f06fb10df7435
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-11-01 14:59:42 +0300

    Day 29 — Introducing List, your best friend

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        List {
+//            Section("Section 1") {
+//                Text("Static row 1")
+//                Text("Static row 2")
+//            }
+//
+//            Section("Section 2") {
+//                ForEach(0..<5) {
+//                    Text("Dynamic row \($0)")
+//                }
+//            }
+//
+//            Section("Section 3") {
+//                Text("Static row 3")
+//                Text("Static row 4")
+//            }
+//
+////            Text("Hello, world!")
+////            Text("Hello, world!")
+////            Text("Hello, world!")
+//        }
+//        .listStyle(.grouped)
+        
+//        List(0..<5) {
+//            Text("Dynamic row \($0)")
+//        }
+        
+//        List(people, id: \.self) {
+//            Text($0)
+//        }
+        
+        List {
+            Text("Static row")
+            
+            ForEach(people, id: \.self) {
+                Text($0)
+            }
+            
+            Text("Static row")
+        }
+        .listStyle(.grouped)
     }
 }
 
